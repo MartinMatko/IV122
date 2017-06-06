da224104c907b9cb4c098f99f74f4edc09d686ce
MartinMatko <matias93@centrum.sk>
2017-06-06 14:39:34 +0200

    finish
    finish

--- Lsystem.py
+++ Lsystem.py
@@ -6,17 +6,15 @@ class Lsystem:
         self.variable = variable
         self.rules = rules
         self.axioms = axioms
-        self.expanded = self.variable
         self.turtle = Turtle()
 
-    def expand(self, depth):
+    def draw(self, depth, fileName):
+        expanded = self.variable
         for i in range(depth):
             for rule in self.rules:
-                self.expanded = self.expanded.replace(rule, self.rules[rule])
-
-    def draw(self, depth, fileName):
-        self.expand(depth)
-        for i in self.expanded:
+                expanded = expanded.replace(rule, self.rules[rule])
+                
+        for i in expanded:
             if i in self.axioms:
                 exec("self.turtle." + self.axioms[i])
         self.turtle.draw(fileName)
